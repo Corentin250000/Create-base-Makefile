33a1a2b64ca4352082c6b41bfc4ec3585bb4c640
Corentin PASQUIER <corentin.pasquier@epitech.eu>
2024-08-07 15:33:25 +0200

    [+] Add rule for unit tests

--- create_makefile.sh
+++ create_makefile.sh
@@ -9,7 +9,11 @@ echo -e $HEADER > $FILE
 
 echo -e "\nSRC\t=" >> $FILE
 
+echo -e "\nSRC_UNIT_TESTS\t=" >> $FILE
+
 echo -e "\nOBJ\t=\t$""(SRC:.c=.o)" >> $FILE
+
+echo -e "\nOBJ_UNIT_TESTS\t=\t$""(SRC_UNIT_TESTS:.c=.o)" >> $FILE
 
 echo -e "\nNAME\t=" >> $FILE
 
@@ -19,7 +23,9 @@ echo -e "\nFLAGS\t=\t-Wall -Wextra -g" >> $FILE
 
 echo -e "\nCFLAGS\t=" >> $FILE
 
-echo -e "\n$""(NAME):\n\t\t$""(CC) -o $""(NAME) $""(SRC) $""(FLAGS) $""(CFLAGS)" >> $FILE
+echo -e "\nLIBS\t=" >> $FILE
+
+echo -e "\n$""(NAME):\n\t\t$""(CC) -o $""(NAME) $""(SRC) $""(FLAGS) $""(CFLAGS) $""(LIBS)" >> $FILE
 
 echo -e "\nall:\t$""(NAME)" >> $FILE
 
@@ -29,6 +35,8 @@ echo -e "\nfclean:\tclean\n\t\trm -f $""(NAME)\n\t\trm -f a.out" >> $FILE
 
 echo -e "\nre:\t\tfclean all" >> $FILE
 
-echo -e "\n.PHONY:\tall clean fclean re" >> $FILE
+echo -e "\ntests_run:\t$""(OBJ_UNIT_TESTS)\n\t\t$""(CC) -o unit_tests $""(OBJ_UNIT_TESTS) --coverage -lcriterion\n\t\t./unit_tests --verbose" >> $FILE
+
+echo -e "\n.PHONY:\tall clean fclean re tests_run" >> $FILE
 
 exit 0
