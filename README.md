467d015f0c4361fec6e367efa42e51220bda1110
Corentin PASQUIER <corentin.pasquier@epitech.eu>
2024-08-12 19:53:12 +0200

    [+] Add correction for unit tests and epitech coding style

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Create-base-Makefile
 
-Ce programme crée la base vide d'un Makefile (adapté à la convention de l'école).
+Ce programme crée la base vide d'un Makefile (adapté à la convention d'Epitech).
 
 ## Comment utiliser le programme ?
 
-Téléchargez le script shell nommé **create_makefile.sh**, et exécutez-le dans le dossier où vous voulez mettre le Makefile. Si vous n'avez pas le droit d'exécution sur le fichier, ajoutez-le via la commande `chmod +x create_makefile.sh`.
+Téléchargez le script shell nommé **create_makefile.sh**, et exécutez-le (`./create_makefile.sh`) dans le dossier où vous voulez mettre le Makefile. Si vous n'avez pas le droit d'exécution sur le fichier, ajoutez-le via la commande `chmod +x create_makefile.sh`.
 
-Pour les plus avancés, vous pouvez ajouter un chemin (**PATH**) dans votre fichier **.bashrc** (si bash utilisé, pour les autres, renseignez-vous) avec cette commande : `export PATH=$PATH:` avec, après le ':', le chemin absolu vers votre dossier où se trouve le script. Cela vous permettra de pouvoir exécuter le script où que vous soyez dans vos dossiers.
+Pour les plus avancés, vous pouvez ajouter un chemin (**PATH**) dans votre fichier **.bashrc** (si bash utilisé, pour les autres, renseignez-vous) avec cette commande : `export PATH=$PATH:` avec, après le ':', le chemin absolu vers votre dossier où se trouve le script. Cela vous permettra de pouvoir exécuter le script où que vous soyez dans vos dossiers (commmande à exécuter : `create_makefile.sh`).
 
 **Attention, l'exécution du script effacera le précédent Makefile, si présent.**
--- create_makefile.sh
+++ create_makefile.sh
@@ -3,7 +3,10 @@
 # This script create an empty Makefile adapted to the Epitech convention.
 
 FILE="Makefile"
-HEADER="##\n## EPITECH PROJECT, 2024\n## "$FILE"\n## File description:\n## "$FILE"\n##"
+
+YEAR=$(date | awk '{print $7}')
+
+HEADER="##\n## EPITECH PROJECT, "$YEAR"\n## "$FILE"\n## File description:\n## "$FILE"\n##"
 
 echo -e $HEADER > $FILE
 
@@ -21,22 +24,30 @@ echo -e "\nCC\t=\tgcc" >> $FILE
 
 echo -e "\nFLAGS\t=\t-Wall -Wextra -g" >> $FILE
 
-echo -e "\nCFLAGS\t=" >> $FILE
+echo -e "\nCFLAGS\t=\t-fprofile-arcs -ftest-coverage -O0" >> $FILE
 
-echo -e "\nLIBS\t=" >> $FILE
+echo -e "\nLIBS\t=\t--coverage -lcriterion" >> $FILE
 
 echo -e "\n$""(NAME):\n\t\t$""(CC) -o $""(NAME) $""(SRC) $""(FLAGS) $""(CFLAGS) $""(LIBS)" >> $FILE
 
 echo -e "\nall:\t$""(NAME)" >> $FILE
 
-echo -e "\nclean:\n\t\trm -f $""(OBJ)" >> $FILE
+echo -e "\nclean:\n\t\trm -f $""(OBJ)\n\t\trm -f $""(OBJ_UNIT_TESTS)\n\t\tfind . -type f -name '*.gcda' -exec rm -f {} +\n\t\tfind . -type f -name '*.gcno' -exec rm -f {} +\n\t\tfind . -type f -name '*.gcov' -exec rm -f {} +" >> $FILE
 
-echo -e "\nfclean:\tclean\n\t\trm -f $""(NAME)\n\t\trm -f a.out" >> $FILE
+echo -e "\nfclean:\tclean\n\t\trm -f $""(NAME)\n\t\trm -f a.out\n\t\trm -f unit_tests\n\t\trm -f coverage.info\n\t\trm -f coverage_filtered.info\n\t\trm -rf out\n\t\trm -f vgcore.*" >> $FILE
 
 echo -e "\nre:\t\tfclean all" >> $FILE
 
-echo -e "\ntests_run:\t$""(OBJ_UNIT_TESTS)\n\t\t$""(CC) -o unit_tests $""(OBJ_UNIT_TESTS) --coverage -lcriterion\n\t\t./unit_tests --verbose" >> $FILE
+echo -ne "\ntests_run:\t$""(OBJ_UNIT_TESTS)\n\t\t$""(CC) -o unit_tests $""(OBJ_UNIT_TESTS) $""(FLAGS) $""(CFLAGS) $""(LIBS)\n\t\t./unit_tests --verbose\n\t\tlcov --capture --directory . --output-file coverage.info" >> $FILE
+echo -e " \\" >> $FILE
+echo -ne "\t\t--ignore-errors mismatch\n\t\tlcov --remove coverage.info '*/tests/*' --output-file" >> $FILE
+echo -e " \\" >> $FILE
+echo -e " \t\tcoverage_filtered.info --ignore-errors mismatch\n\t\tgenhtml coverage_filtered.info --output-directory out" >> $FILE
 
-echo -e "\n.PHONY:\tall clean fclean re tests_run" >> $FILE
+echo -e "\n# Cette règle ne s'adresse qu'aux étudiants Epitech possédant le script" >> $FILE
+
+echo -e "\ncoding_style:\tfclean\n\t\t\tcoding-style . .\n\t\t\tcat coding-style-reports.log\n\t\t\trm -f coding-style-reports.log" >> $FILE
+
+echo -e "\n.PHONY:\tall clean fclean re tests_run coding_style" >> $FILE
 
 exit 0
